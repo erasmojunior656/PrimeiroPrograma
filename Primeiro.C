a5d5a2d4b501100290ae44113a6fef84d42ae466
erasmojunior656 <j.erasmo@escola.pr.gov.br>
2025-02-20 12:08:43 -0300

    Add files via upload

---
+++ Primeiro.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int erasmo;
+    int stevan;
+    int soma;
+    int multiplicacao;
+    erasmo = 15;
+    stevan = 10;
+    soma = erasmo + stevan;
+    multiplicacao = erasmo * stevan;
+    printf("o valor de erasmo é %d \n", erasmo);
+    printf("o valor de stevan é %d \n", stevan);
+    printf("a soma é %d \n", soma);
+    printf("a multiplicação é %d", multiplicacao);
+
+    return 0;
+}
